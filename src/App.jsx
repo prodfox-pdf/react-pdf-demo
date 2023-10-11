74afe1acea51cbc1bdfb249d6659c12e0ca16a59
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-10-11 08:12:06 -0400

    rm unused code

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,11 @@ import { useCreateIframeAndLoadViewer } from "pdf_editor_aleon35_react_plugin";
 function App() {
   const containerRef = useRef(null);
 
-  /*
-  const { download } = useCreateIframeAndLoadViewer({
-    container: containerRef,
-    fileName: "my-file.pdf",
-    licenseKey: "sandbox",
-    locale: "en",
-    tools: {
-      editing: [
-        "remove",
-        "rotation"
-      ],
-      thumbnails: [
-        "zoom",
-        "expand"
-      ],
-      general: [
-        "zoom",
-        "search",
-        "download",
-        "thumbnails"
-      ],
-    },
-    file: "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf" || /*"https://edepot.wur.nl/633278" "https://api.printnode.com/static/test/pdf/multipage.pdf"
-  });
-  */
-
   const { download } = useCreateIframeAndLoadViewer({
     container: containerRef,
     fileName: "my-file.pdf",
     uuid: "some-user",
-    licenseKey: "sandbox" || "f4470da7-1552-4a1c-b410-c6939faf5854",
+    licenseKey: "sandbox",
     locale: "en",
     tools: {
       editing: [
