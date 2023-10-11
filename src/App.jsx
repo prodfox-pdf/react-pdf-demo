b96668e5db495bb600095829212bc7baa642fac8
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-10-11 08:09:31 -0400

    w

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useCreateIframeAndLoadViewer } from "pdf_editor_aleon35_react_plugin";
 function App() {
   const containerRef = useRef(null);
 
+  /*
   const { download } = useCreateIframeAndLoadViewer({
     container: containerRef,
     fileName: "my-file.pdf",
@@ -25,8 +26,51 @@ function App() {
         "thumbnails"
       ],
     },
-    file: "https://api.printnode.com/static/test/pdf/multipage.pdf"
+    file: "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf" || /*"https://edepot.wur.nl/633278" "https://api.printnode.com/static/test/pdf/multipage.pdf"
   });
+  */
+
+  const { download } = useCreateIframeAndLoadViewer({
+    container: containerRef,
+    fileName: "my-file.pdf",
+    uuid: "some-user",
+    licenseKey: "sandbox" || "f4470da7-1552-4a1c-b410-c6939faf5854",
+    locale: "en",
+    tools: {
+      editing: [
+        // @ts-ignore
+       "extract",
+       "remove",
+       // @ts-ignore
+        "move"
+      ],
+      thumbnails: [
+        "zoom",
+        "expand"
+      ],
+      general: [
+        "thumbnails",
+        "download",
+        "search",
+        "panel-toggle",
+        "zoom",
+        // @ts-ignore
+        "chat"
+      ],
+    },
+    files: [
+      {
+        url: "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf",
+        name: "my-file1.pdf"
+      },
+      {
+        url: "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf",
+        name: "my-file2.pdf"
+      }
+    ],
+    // file: /*"http://localhost:3000/pdfs/54f860c9-a945-46e6-b211-41e4b5497e94.pdf" ||*/ "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf"
+  });
+
 
   return (
     <div>
